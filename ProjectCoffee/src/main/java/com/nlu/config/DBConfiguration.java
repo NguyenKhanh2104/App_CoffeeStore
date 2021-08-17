482c91953f801c25b77bf59597faab718b363e70
MYCOMPUTER\Admin <khanhnguyen7221@gmail.com>
2021-08-17 20:37:51 +0700

    check upcode

--- ProjectCoffee/src/main/java/com/nlu/config/DBConfiguration.java
+++ ProjectCoffee/src/main/java/com/nlu/config/DBConfiguration.java
@@ -3,10 +3,18 @@ package com.nlu.config;
 import java.sql.*;
 
 public class DBConfiguration {
-    public static final String DB_USERNAME = "root";
+        public static final String DB_USERNAME = "root";
     public static final String DB_PASSWORD = "";
     public static final String DB_URL = "jdbc:mysql://localhost/test?useUnicode=true;characterEncoding=UTF-8";
     public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final int DB_MAX_CONNECTION = 100;
+//    public static final String DB_USERNAME = "iRhuKBkTcg";
+//    public static final String DB_PASSWORD = "HcfcUZ7K9P";
+//    public static final String DB_URL = "jdbc:mysql://mysql/iRhuKBkTcg?useUnicode=true;characterEncoding=UTF-8";
+////    public static final String DB_DRIVER = "remotemysql.com";
+//    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+//    public static final int DB_MAX_CONNECTION = 100;
+
+
 
 }
